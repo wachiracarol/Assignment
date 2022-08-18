d2e8d0d5b10fc104928002b9316d2e280d2ab813
wachiracarol <109791888+wachiracarol@users.noreply.github.com>
2022-08-18 16:03:39 +0300

    Add files via upload

---
+++ Caroline Wachira Assignment.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Caroline Muthoni Wachira**
+# 
+# Medical  Data Scientist
+
+# *I am interested in Data Science escpecialy in the field of Healthcare for prediction of disease  surveillance*
+
+# ### Disease Prevalnce 
+
+# In[5]:
+
+
+1+1
+
+
+# https://skills.network/
+# 
+# | Disease | Percentage  |
+# | ------ | ------ |
+# | Pneumonia | 23% |
+# *  
+#     *  
+# 1.  
+# 2.  
+
+# In[ ]:
+
+
+
+
